75606ce1794f8142599b3a653ce9f9e831c01774
Nitish Kumar Gupta <57590993+nitishgupta08@users.noreply.github.com>
2023-05-30 05:07:36 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,16 @@ import Contacts from "./screens/Contact";
 import Footer from "./screens/Footer";
 import { Routes, Route } from "react-router-dom";
 import ProjectArchive from "./screens/ProjectArchive";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+
+ReactGA.initialize(import.meta.env.VITE_TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
   return (
     <div className="bg-bgDark">
       <Routes>
