b2bc39516c2dd0b7383a2941873450ce30a8bd7f
Nitish Kumar Gupta <nitishkg.88@gmail.com>
2023-06-27 19:49:24 +0530

    bug fix #2

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,21 @@ import Contacts from "./screens/Contact";
 import Footer from "./screens/Footer";
 import { Routes, Route } from "react-router-dom";
 import ProjectArchive from "./screens/ProjectArchive";
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize(import.meta.env.VITE_GA4);
+  ReactGA.send({
+    hitType: "pageview",
+    page: "/",
+    title: "Landing Page",
+  });
+  ReactGA.send({
+    hitType: "pageview",
+    page: "/project-archive",
+    title: "Project Archive Page",
+  });
+
   return (
     <div className="bg-bgDark">
       <Routes>
