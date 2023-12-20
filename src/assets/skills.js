e61ea9a9f0df2255866472924d9c43d6269137f8
Nitish Kumar Gupta <57590993+nitishgupta08@users.noreply.github.com>
2023-12-20 22:53:18 +0530

    Update skills.js

--- src/assets/skills.js
+++ src/assets/skills.js
@@ -1,11 +1,8 @@
 export const languages = ['C', 'C++', 'Python', 'Javascript', 'HTML', 'CSS', 'SQL', 'Bash', 'Typescript', 'Java']
 
-export const tech = ['Django', 'DRF', 'ReactJS', 'NodeJS', 'Redis']
+export const tech = ['Django', 'DRF', 'ReactJS', 'NodeJS', 'Redis', 'Socket.io', 'Express', 'MUI', 'TailwindCSS', 'React Router', 'Redux']
 
 export const tools = ['Git & Github', 'Postman', 'MongoDB', 'Vite', 'Apache']
-
-export const packages = ['Socket.io', 'Express', 'MUI', 'TailwindCSS', 'React Router', 'Redux']
-
 
 export const dsa = [
     { name: 'Leetcode', link: 'https://leetcode.com/nitishgupta24/' },
